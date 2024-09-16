ab1ed120aa32b2a77252a5a9bc6851e7da7239e2
4th-avenue <quebec706@gmail.com>
2024-09-16 16:19:33 +0900

    x-model

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
-        <script src="https://cdn.tailwindcss.com"></script>
+        <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp,container-queries"></script>
         @vite('resources/js/app.js')
     </head>
     <body class="font-sans antialiased dark:bg-black dark:text-white/50">
@@ -100,8 +100,7 @@
                                 </div>
                             </a>
 
-                            <a
-                                href="https://laracasts.com"
+                            <div
                                 class="flex items-start gap-4 rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]"
                             >
                                 <div class="flex size-12 shrink-0 items-center justify-center rounded-full bg-[#FF2D20]/10 sm:size-16">
@@ -109,15 +108,24 @@
                                 </div>
 
                                 <div class="pt-3 sm:pt-5">
-                                    <h2 class="text-xl font-semibold text-black dark:text-white">Laracasts</h2>
+                                    <h2 class="text-xl font-semibold text-black dark:text-white">x-model</h2>
 
-                                    <p class="mt-4 text-sm/relaxed">
-                                        Laracasts offers thousands of video tutorials on Laravel, PHP, and JavaScript development. Check them out, see for yourself, and massively level up your development skills in the process.
-                                    </p>
+                                    <div x-data="{ message: 'Hello, mystee!' }" class="my-2 space-y-2">
+                                        <input type="text" x-model="message">
+                                        <p x-text="message" class="p-2 bg-slate-200"></p>
+                                    </div>
+                                    <div class="max-w-md mx-auto space-y-2" x-data="{ myColor: 'gray' }">
+                                        <div class="h-12 rounded" :class="'bg-' + myColor + '-500'"></div>
+                                        <select x-model="myColor">
+                                            <option>--select--</option>
+                                            <option value="rose">Rose</option>
+                                            <option value="amber">Amber</option>
+                                            <option value="fuchsia">Fuchsia</option>
+                                            <option value="lime">Lime</option>
+                                        </select>
+                                    </div>
                                 </div>
-
-                                <svg class="size-6 shrink-0 self-center stroke-[#FF2D20]" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"><path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"/></svg>
-                            </a>
+                            </div>
 
                             <div
                                 class="flex items-start gap-4 rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]"
